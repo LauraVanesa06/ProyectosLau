3d7c1d7818fc5f26962ba2828c4955af76d26652
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2024-11-29 06:18:31 -0500

    ...

--- ACTIVIDADESDELADSO2928797/HTML/27-11-2024/prueba.js
+++ ACTIVIDADESDELADSO2928797/HTML/27-11-2024/prueba.js
@@ -1,19 +1,26 @@
 var id = document.getElementById('id')
 var button = document.getElementById('button')
-button.addEventListener('click', function(){
-  var inputId = id.value;
 
-  fetch('https://jsonplaceholder.typicode.com/todos/' + inputId)
-  .then(response => response.json())
-  .then(json => {
-    var a = document.querySelector("body > center > main > table > tbody > tr:nth-child(2) > td:nth-child(2)");
-    a.innerHTML = json.id;
 
-    var b =document.querySelector("body > center > main > table > tbody > tr:nth-child(3) > td:nth-child(2)");
-    b.innerHTML = json.title;
+button.addEventListener("click", 
+  function buscar() {  
 
-    var c = document.querySelector("body > center > main > table > tbody > tr:nth-child(4) > td:nth-child(2)")
-    c.innerHTML = json.completed;
+    var inputId = id.value;
+    
+    fetch('https://jsonplaceholder.typicode.com/todos/'+inputId)
+      .then(response => response.json())
+      .then(obj => {
+        var a = document.querySelector("body > center > table > tbody > tr:nth-child(2) > td:nth-child(2)");
+        a.innerHTML = obj.id;
+        
+        var b = document.querySelector("body > center > table > tbody > tr:nth-child(3) > td:nth-child(2)");
+        b.innerHTML = obj.title;
 
-  })
-})
+        var c = document.querySelector("body > center > table > tbody > tr:nth-child(4) > td:nth-child(2)");
+        c.innerHTML = obj.completed;
+    }
+      );
+
+
+  }
+)
